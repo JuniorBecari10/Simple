932992ddc1687557b2f2a908b0927f8c6ed7d26a
Antônio Carlos <juniorbecari@gmail.com>
2022-11-05 20:36:12 -0300

    Adding tokens

--- lang.py
+++ lang.py
@@ -1,17 +1,26 @@
 import sys
 
-token_types = [  # Examples: (that's the one with brackets)
-  "var",         # [a] = 10
-  "keyword",     # [print]
-  "assign",      # a [=] 10
-  "value",       # a = [10]
-  "var_ref"      # print [$a]
-]
+token_types = {          # Examples: (that's the one with brackets)
+  "var": "",             # [a] = 10
+  "keyword": ["print"],  # [print]
+  "assign": "=",         # a [=] 10
+  "value": "",           # a = [10]
+  "var_ref": "$",        # print [$a]
+}
 
 class Token:
+  type = ""
+  content = ""
+  
   def __init__(self, t, c):
     self.type = t
     self.content = c
+  
+  def __str__(self):
+    return "Token: type: " + self.type + ", content: " + self.content
+  
+  def __repr__(self):
+    return "Token: type: " + self.type + ", content: " + self.content
 
 # ---
 
@@ -21,19 +30,37 @@ def main():
     sys.exit(1)
   
   try:
-    f = open(sys.argv[1], "r")
+    with open(sys.argv[1], "r") as f:
+      lines = f.readlines()
+      tokens = lexer(lines)
+      
+      print(tokens)
   except FileNotFoundError:
     print(f"The file '{sys.argv[1]}' doesn't exist.")
     sys.exit(1)
-  
-  lines = f.readlines()
-  tokens = lexer(lines)
 
 # ---
 
-# Return: a list of tokens
+# Return: a list of list of tokens (bidimensional array)
 def lexer(lines):
+  tokens = []
   
+  for line in lines:
+    tk_char = line.split(" ")
+    tks = []
+    
+    for ch in tk_char:
+      # verify if 'ch' is a equals sign
+      if ch == token_types["assign"]:
+        tks.append(Token("assign", ch))
+      # verify if is a variable (if it contains an equals sign in the line)
+      elif line.__contains__(token_types["assign"]):
+        tks.append(Token("var", ch))
+      
+    
+    tokens.append(tks)
+  
+  return tokens
 
 # ---
 
