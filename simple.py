fc2f6fc9fd1a8d2a49e73a071441587e5f347df6
Antônio Carlos <juniorbecari@gmail.com>
2022-11-26 14:50:51 -0300

    Finished commenting

--- simple.py
+++ simple.py
@@ -98,11 +98,13 @@ def lexer(lines):
     tk_char = line.split(" ")
     tks = []
     
+    # Read word by word
     for i, ch in enumerate(tk_char):
+      # Don't add the comments to the token list
       if ch == token_types["comment"]:
         break
       
-      # skip if is nothing
+      # skip if there's nothing
       if ch == "":
         continue
       elif ch in token_types["keyword"]:
@@ -140,6 +142,7 @@ def lexer(lines):
           tks.append(Token("var", ch))
       # else it's text
       else:
+        # If the lexer can't define correctly, there's a fallback
         if "if" in line:
           if i == 1:
             tks.append(Token("var", ch))
@@ -149,28 +152,40 @@ def lexer(lines):
         
         tks.append(Token("text", ch))
     
+    # append the list to the tokens 2d list
     tokens.append(tks)
   
   return tokens
 
+# Add the labels by reading the entire file first
 def add_labels(tokens):
   for lc, line in enumerate(tokens):
     for i, t in enumerate(line):
-        if t.type == "label":
-          if i == 0:
-            labels[t.content[1:]] = lc + 1
-          else:
-            line_str = " ".join(token_to_str(line))
-            
-            if not line_str.__contains__("goto"):
-              throw_error("A label must be the first token in line!", lc + 1)
+      # if it's a label
+      if t.type == "label":
+        # Labels must be the first token in line. See the error message.
+        if i == 0:
+          # If yes, append to the labels dictionary
+          labels[t.content[1:]] = lc + 1
+        else:
+          line_str = " ".join(token_to_str(line))
+          
+          # If not, it must be with a goto statement.
+          if not line_str.__contains__("goto"):
+            # If not, throw an error.
+            throw_error("A label must be the first token in line!", lc + 1)
 
+# Run the code
 def run(tokens):
+  # Define the iterator
   it = Iterator(0, len(tokens))
   
+  # Loop through it
   for line_count in it:
+    # Define as 'line' the current line it's iterating over
     line = tokens[line_count]
     
+    # If there's nothing, continue
     if len(line) == 0:
       continue
     
@@ -186,58 +201,76 @@ def run(tokens):
         except Exception:
           throw_error(f"Variable '{line[i].content[1:]}' doesn't exist.", i + 1)
       
+      # Try to convert to number. If not, pass.
       try:
         line[i].content = float(line[i].content)
         line[i].type = "value"
       except Exception:
         pass
     
+    # Verify if the current line is a variable declaration
     if is_var_decl(line):
+      # The value is the third (or second, counting up from 0) element.
+      # var = [value]
+      #  0  1    2
       value = line[2].content
       
+      # Verify if the 'value' is the keyword 'input'
       if line[2].type == "keyword" and line[2].content == "input":
+        # Loop until the user types the desired type
         while True:
-            value = input("")
+          value = input("")
+          
+          # If the programmer only types 'input', break, because the type doesn't matter
+          if len(line) == 3:
+            break
+          
+          # if not, proceed.
+          if len(line) == 4:
+            # Verify each one of the types
+            if line[3].content == "num":
+              try:
+                _ = float(value)
+                
+                break
+              except Exception:
+                continue
+            elif line[3].content == "str":
+              try:
+                _ = float(value)
             
-            if len(line) == 3:
-              break
-            
-            if len(line) == 4:
-              if line[3].content == "num":
-                try:
-                  _ = float(value)
-                  
-                  break
-                except Exception:
-                  continue
-              elif line[3].content == "str":
-                try:
-                  _ = float(value)
-              
-                  continue
-                except Exception:
-                  break
-              else:
+                continue
+              except Exception:
+                break
+            else:
                 throw_error("Type '" + line[3].content + "' is not allowed for input.", line_count + 1)
       
+      # Add the variable.
       add_variable(line[0].content, value)
+    # Verify if the programmer typed something wrong
     elif line[0].type == "var" and line[1].type != "math":
       throw_error("Syntax error while declaring a variable.", line_count + 1)
     
+    # Verify if it's a mathematical operation
     if is_var_math(line):
+      # Get variables
       var1 = variables[line[0].content]
       var2 = line[2].content
       
+      # Convert to num if possible
       try:
         var1 = float(var1)
       except Exception:
         var1 = variables[line[0].content]
       
+      # Prevention to not do math operations with distinct variable types besides plus
       if type(var1) != type(var2) and not line[1].content.startswith("+"):
         throw_error("Variable types don't match.", line_count + 1)
       
+      # Variable to check if both of them are num's
       is_num = type(var1) is float and type(var2) is float
       
+      # Plus. With str's you can do, the effect will be a concatenation
       try:
         if line[1].content.startswith("+"):
           if type(var1) != type(var2):
@@ -245,6 +278,7 @@ def run(tokens):
             var2 = str(var2)
           
           variables[line[0].content] = var1 + var2
+        # Minus and others. These cannot be done with str's.
         elif line[1].content.startswith("-") and is_num:
           variables[line[0].content] = var1 - var2
         elif line[1].content.startswith("*") and is_num:
@@ -265,21 +299,27 @@ def run(tokens):
     elif len(line) > 1 and line[1].type == "math":
       throw_error("Syntax error while doing a math operation.", line_count + 1)
     
+    # Operations with keywords
     if line[0].type == "keyword":
+      # Print statement
       if line[0].content.startswith("print"):
+        # Loop all through the line and print each one of them
         for i, t in enumerate(line):
           if i == 0:
             continue
           
           print(t.content, end=" ")
         
+        # If the keyword is printl, don't break the line
         if line[0].content != "printl":
           print()
+      # Goto statement
       elif line[0].content == "goto":
         try:
           if len(line) != 2:
             throw_error("Syntax error on a goto statement.", line_count + 1)
           
+          # Check if the user typed a line number or label
           if line[1].type == "value":
             # must be int
             line_go = int(line[1].content)
@@ -288,26 +328,33 @@ def run(tokens):
           else:
             raise Exception()
           
+          # Check if the line is inside the bounds of the file
           if line_go < 0 or line_go > len(tokens):
             throw_error(f"Line out of bounds: {line_go}", line_count + 1)
           
+          # Do the 'goto'
           it.revert(line_go - 1)
         except Exception:
           throw_error("Couldn't parse the line number or label to go. Value read: " + line[1].content, line_count + 1)
+      # Exec statement
       elif line[0].content == "exec":
         if len(line) == 1:
           throw_error("No commands to run.", line_count + 1)
         
         com = []
         
+        # Loop through the line and add to the command list
         for i, t in enumerate(line):
           if i == 0:
             continue
           
           com.append(t.content)
         
+        # Run
         os.system(" ".join(com))
+      # Exit
       elif line[0].content == "exit":
+        # Get status code
         if len(line) == 2:
           try:
             # must be int
@@ -315,14 +362,18 @@ def run(tokens):
           except Exception:
             throw_error("Invalid status code.", line_count + 1)
           
+          # Exit
           sys.exit(status)
+      # Condition: if statement
       elif is_condition(line):
         try:
+          # Get values
           var = variables[line[1].content]
           value = line[3].content
           oper = line[2].content
           line_go = line[5].content
           
+          # Check if the value typed for goto is a line or a label
           try:
             if line[5].type == "value":
               # must be int
@@ -339,6 +390,7 @@ def run(tokens):
           var_num = True
           value_num = True
           
+          # Convert the values to num
           try:
             var = float(var)
           except Exception:
@@ -351,11 +403,13 @@ def run(tokens):
           
           is_num = var_num and value_num
           
+          # Do the checks, based on the operator the programmer has typed
           if oper == "==":
             go = str(var) == str(value)
           elif oper == "!=":
             go = str(var) != str(value)
           
+          # Below here, only num's can perform these logic operations
           if not is_num and oper in token_types["mathlogic"]:
             throw_error(f"Cannot perform math logical operations on strings.", line_count + 1)
           
@@ -368,6 +422,7 @@ def run(tokens):
           elif oper == "<=":
             go = var <= value
           
+          # If succeeded, go to the specified line
           if go:
             if line_go < 0 or line_go > len(tokens):
               throw_error(f"Line out of bounds: {line_go}", line_count + 1)
@@ -377,6 +432,7 @@ def run(tokens):
         except Exception:
           throw_error(f"Variable '{line[1].content}' doesn't exist.", line_count + 1)
 
+# Function to check if a value is a num or not
 def is_num(var):
   try:
     _ = float(variables[var])
@@ -384,6 +440,7 @@ def is_num(var):
   except Exception:
     return False
 
+# Function to convert a list of Tokens to a list of strs (t.content)
 def token_to_str(tokens):
   strs = []
   
@@ -392,6 +449,7 @@ def token_to_str(tokens):
   
   return strs
 
+# Function to ckeck if an array is inside another
 def contains_arr(line, arr):
   for n in arr:
     if line.__contains__(n):
@@ -399,6 +457,7 @@ def contains_arr(line, arr):
   
   return False
 
+# Functions to check certain situations
 def is_var_math(tokens):
   return len(tokens) == 3 and tokens[0].type == "var" and tokens[1].type == "math" and (tokens[2].type == "value" or tokens[2].type == "var_ref")
 
@@ -408,9 +467,13 @@ def is_var_decl(tokens):
 def is_condition(tokens):
   return len(tokens) == 6 and tokens[0].type == "keyword" and tokens[1].type == "var" and tokens[2].type == "logic" and (tokens[3].type == "value" or tokens[3].type == "var_ref") and tokens[4].type == "keyword" and (tokens[5].type == "value" or tokens[5].type == "label")
 
+# ---
+
+# Function to add a variable
 def add_variable(name, value):
   variables[name] = value
 
+# Function used to throw errors
 def throw_error(message, line_number):
   line_number = str(line_number)
   
@@ -422,6 +485,7 @@ def throw_error(message, line_number):
   
   sys.exit(1)
 
+# Function to throw error without specifing the line
 def throw_error_noline(message):
   print("-----")
   print("ERROR\n")
@@ -432,5 +496,6 @@ def throw_error_noline(message):
 
 # ---
 
+# Main code
 if __name__ == "__main__":
   main()
