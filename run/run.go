327f84110a59b98e222c83e63273c0c0477000fb
Antônio Carlos <juniorbecari@gmail.com>
2023-02-23 13:30:34 -0300

    Continuing

--- run/run.go
+++ run/run.go
@@ -1,10 +1,18 @@
 package run
 
 import (
+  "fmt"
+  
   "simple/ast"
 )
 
-var Variables = map[string]string {}
+type Any interface {}
+
+type Value struct {
+  Value Any
+}
+
+var Variables = map[Any]Any {}
 
 func Run(stats []ast.Statement) {
   pc := 0
@@ -17,7 +25,7 @@ func Run(stats []ast.Statement) {
   }
 }
 
-func RunStat(stat ast.Statement, repl bool) string {
+func RunStat(stat ast.Statement, repl bool) Any {
   fn := GetStatFunc(stat)
   
   if fn == nil {
@@ -27,7 +35,7 @@ func RunStat(stat ast.Statement, repl bool) string {
   return fn(stat)
 }
 
-func GetStatFunc(st ast.Statement) func(ast.Statement) string {
+func GetStatFunc(st ast.Statement) func(ast.Statement) Any {
   switch st.(type) {
     case VarDeclStatement:
       return RunVarDeclStatement
@@ -40,16 +48,42 @@ func GetStatFunc(st ast.Statement) func(ast.Statement) string {
   }
 }
 
-func RunVarDeclStatement(st ast.VarDeclStatement) string {
-  Variables[st.Name.Value] = 
+func RunVarDeclStatement(st ast.VarDeclStatement) Any {
+  Variables[st.Name.Value] = SolveExpression(st.Value)
 }
 
-func RunPrintStatement(st ast.PrintStatement) string {
+func RunPrintStatement(st ast.PrintStatement) Any {
   
 }
 
 // ---
 
-func SolveExpression(ex ast.ExpressionNode) string {
+func SolveExpression(ex ast.ExpressionNode) Any {
+  fn := GetExprFunc(ex)
   
+  if fn == nil {
+    return "ERROR: Couldn't get function to solve this expression: " + fmt.Sprintf("%q", ex)
+  }
+  
+  return fn(ex)
+}
+
+func GetExprFunc(ex ast.ExpressionNode) func(ast.ExpressionNode) Any {
+  switch ex.(type) {
+    case Identifier:
+      return func(ex ast.ExpressionNode) Any {
+        return ex.(Identifier).Value
+      }
+    
+    case NumberNode:
+      return func(ex ast.ExpressionNode) Any {
+        return ex.(NumberNode).Value
+      }
+    
+    case Identifier:
+      return func(ex ast.ExpressionNode) Any {
+        return ex.(Identifier).Value
+      }
+    
+  }
 }
